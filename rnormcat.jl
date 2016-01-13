73d6a79413757892c833cc9aa61f870c50e3d083
thewart <seth.madlonkay@gmail.com>
2016-01-12 19:02:51 -0500

    bug fixes

--- rnormcat.jl
+++ rnormcat.jl
@@ -1,20 +1,21 @@
 function rnormalprobit(n,d,se,nsamp=1000)
-  pi = zeroes(Float64,(d,n));
+  pi = zeros((d,n));
   eta = Array(Float64,d);
   w = Array(Float64,d);
   for i in 1:n
-    rand!(Normal(0,se),eta);
+    rand!(Normal(),eta);
     for j in 1:nsamp
       for k in 1:d w[k] = eta[k] + rand(Normal(0,se)); end
       c = findmax(w)[2];
       pi[c,i] += 1/nsamp
     end
   end
-  return y
+  return pi
 end
 
 function rnormallogit(n,d,se)
   w = Array(Float64,d);
+  pi = Array(Float64,(d,n));
   for i in 1:n
     rand!(Normal(0,se),w);
     pi[:,i] = exp(w) ./ sum(exp(w));
