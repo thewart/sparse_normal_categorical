afb4a6e014a528858435761235e21ff500e2e547
Seth Madlon-Kay <seth.madlonkay@gmail.com>
2016-02-12 00:43:27 -0500

    complete but untested

--- normal_probit_gibbs.jl
+++ normal_probit_gibbs.jl
@@ -5,33 +5,61 @@ y = [rand(Poisson(1),div(n,3)); rand(Poisson(3),div(n,3));rand(Poisson(5),div(n,
 X = ones(n);
 
 
-function psbpm(y,X,loglik,prior,burn,thin,iter)
+function psbpm(y,X,loglik,rtheta,prior,burn,thin,iter)
 
   #priors
-  K = 10;
-  a0 = 1;
-  b0 = 1/3;
-  SigmaB0 = eye(1);
-  muB0 = 0;
+  K = prior[:K];
+  #a0 = 1;
+  #b0 = 1/3;
+  #SigmaB0 = eye(1);
+  #muB0 = 0;
 
   #initialize
+  saveiter = (burn+1):thin:iter;
+  nsave = length(saveiter);
+  niter = maximum(saveiter);
+
   samples = Dict{Symbol,Array{Float64}}();
-  z = Array(Int64,n);
+  samples[:theta] = Array{Float64}(
+    tuple(vcat(collect(size(theta0)),nsave)...));
+  samples[:eta] = Array{Float64}(K-1,nsave);
+  samples[:z] = Array{Int64}(n,nsave);
+
+  z = Array{Int64}(n);
+  u = Array{Float64}
   eta = rand(Normal(),K-1)/2;
 
   #precompute some regression shit
-  SigmaB = inv( inv(SigmaB0) + X'X );
+  SigmaB = inv( inv(prior[:SigmaB0]) + X'X );
 
   #main loop
   for t in 1:iter
 
+    #sample group memberships
+    sample_z!(z,y,eta,theta,loglik);
+
+    #sample likelihood parameters
+    lambda = rtheta(z,prior);
+
+    #sample latent utilities
+    sample_u!(u,z,eta);
+
+    #sample eta
+    eta = sample_eta(u,X,SigmaB);
+
+    nsamp = findin(saveiter,t);
+    if !isempty(nsamp)
+      samples[:z][:,nsamp] = z;
+      samples[:eta][:,nsamp] = eta;
+      linind = (length(theta)*(nsamp-1)+1):(length(theta)*nsamp);
+      samples[:theta][linind] = theta;
+    end
   end
 
   return samples
 
 end
 
-  #sample group memberships
 function sample_z!(z,y,eta,theta,loglik)
   K = length(eta) + 1;
   n = size(y)[1];
@@ -61,24 +89,24 @@ function sample_z!(z,y,eta,theta,loglik)
 end
 
 #sample poisson rates
-function sample_poisson(z,a0,b0,K)
-  sx = zeros(Int64,K);
-  nx = zeros(Int64,K);
+function sample_poisson(z,prior)
+  K = prior[:K];
+  sz = zeros(Int64,K);
+  nz = zeros(Int64,K);
   n = length(z);
 
   for i in 1:n
     k = z[i];
-    sx[k] += y[i];
-    nx[k] += 1;
+    sz[k] += y[i];
+    nz[k] += 1;
   end
 
   for k in 1:K
-    lambda[k] = rand(Gamma(a0 + sx[k], inv( inv(b0) + nx[k])));
+    lambda[k] = rand(Gamma(prior[:a0] + sx[k], inv(prior[:b0] + nx[k])));
   end
 end
 
-#sample latent utilities
-function sample_u!(u,eta)
+function sample_u!(u,z,eta)
   (K,n) = size(u);
 
   for i in 1:n
