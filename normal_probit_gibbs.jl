f40c47cc798e56d6ce428b34a065647222e63790
thewart <seth.madlonkay@gmail.com>
2016-02-12 19:54:47 -0500

    fully operational and began testing nb

--- normal_probit_gibbs.jl
+++ normal_probit_gibbs.jl
@@ -1,56 +1,52 @@
-using Distributions,Gadfly,StatsFuns
-
-n = 60;
-y = [rand(Poisson(1),div(n,3)); rand(Poisson(3),div(n,3));rand(Poisson(5),div(n,3))];
-X = ones(n);
-
-
 function psbpm(y,X,loglik,rtheta,prior,burn,thin,iter)
 
-  #priors
   K = prior[:K];
-  #a0 = 1;
-  #b0 = 1/3;
-  #SigmaB0 = eye(1);
-  #muB0 = 0;
+  if (isempty(X))
+    X = ones(1,n);
+  end
+  p = size(X)[1];
 
   #initialize
   saveiter = (burn+1):thin:iter;
   nsave = length(saveiter);
   niter = maximum(saveiter);
+  theta = prior[:theta0];
 
   samples = Dict{Symbol,Array{Float64}}();
   samples[:theta] = Array{Float64}(
-    tuple(vcat(collect(size(theta0)),nsave)...));
-  samples[:eta] = Array{Float64}(K-1,nsave);
+    tuple(vcat(collect(size(theta)),nsave)...));
+  samples[:B] = Array{Float64}(p,K-1,nsave);
   samples[:z] = Array{Int64}(n,nsave);
 
   z = Array{Int64}(n);
-  u = Array{Float64}
-  eta = rand(Normal(),K-1)/2;
+  u = Array{Float64}(K-1,n);
+  eta = Array{Float64}(K-1,n);
+  B = rand(Normal(),(p,K-1))/2;
 
   #precompute some regression shit
-  SigmaB = inv( inv(prior[:SigmaB0]) + X'X );
+  SigmaB = inv( inv(prior[:SigmaB0]) + X*X' );
 
   #main loop
   for t in 1:iter
+
+    @into! eta = B'*X;
 
     #sample group memberships
     sample_z!(z,y,eta,theta,loglik);
 
     #sample likelihood parameters
-    lambda = rtheta(z,prior);
+    theta = rtheta(z,prior);
 
     #sample latent utilities
     sample_u!(u,z,eta);
 
-    #sample eta
-    eta = sample_eta(u,X,SigmaB);
+    #sample betas
+    sample_B!(B,u,X,SigmaB);
 
-    nsamp = findin(saveiter,t);
+    nsamp = findin(saveiter,t)[1];
     if !isempty(nsamp)
       samples[:z][:,nsamp] = z;
-      samples[:eta][:,nsamp] = eta;
+      samples[:B][:,:,nsamp] = B;
       linind = (length(theta)*(nsamp-1)+1):(length(theta)*nsamp);
       samples[:theta][linind] = theta;
     end
@@ -61,8 +57,8 @@ function psbpm(y,X,loglik,rtheta,prior,burn,thin,iter)
 end
 
 function sample_z!(z,y,eta,theta,loglik)
-  K = length(eta) + 1;
-  n = size(y)[1];
+  (K,n) = size(eta);
+  K += 1;
   lpkx = Array(Float64,K);
 
   for i in 1:n
@@ -70,14 +66,14 @@ function sample_z!(z,y,eta,theta,loglik)
     for k in 1:K
       #prior weight from psbp
       if k < K
-        lpk = logcdf(Normal(),eta[k]) + lpcum;
-        lpcum += logccdf(Normal(),eta[k]);
+        lpk = logcdf(Normal(),eta[k,i]) + lpcum;
+        lpcum += logccdf(Normal(),eta[k,i]);
       else
         lpk = lpcum;
       end
 
       #likelihood
-      lpx = loglik(y[i],lambda[k]);
+      lpx = loglik(y[i],theta[k]);
 
       lpkx[k] = lpk + lpx;
     end
@@ -94,6 +90,7 @@ function sample_poisson(z,prior)
   sz = zeros(Int64,K);
   nz = zeros(Int64,K);
   n = length(z);
+  lambda = Array(Float64,K);
 
   for i in 1:n
     k = z[i];
@@ -102,35 +99,36 @@ function sample_poisson(z,prior)
   end
 
   for k in 1:K
-    lambda[k] = rand(Gamma(prior[:a0] + sx[k], inv(prior[:b0] + nx[k])));
+    lambda[k] = rand(Gamma(prior[:a0] + sz[k], inv(prior[:b0] + nz[k])));
   end
+
+  return lambda;
 end
 
 function sample_u!(u,z,eta)
   (K,n) = size(u);
-
+  K += 1;
   for i in 1:n
     for k in 1:(K-1)
 
       if k < z[i]
-        u[k,i] = rand(TruncatedNormal(eta[k],1,-Inf,0));
+        u[k,i] = rand(TruncatedNormal(eta[k,i],1,-Inf,0));
       elseif (k == z[i])
-        u[k,i] = rand(TruncatedNormal(eta[k],1,0,Inf));
+        u[k,i] = rand(TruncatedNormal(eta[k,i],1,0,Inf));
       elseif k > z[i]
-        u[k,i] = rand(Normal(eta[k],1));
+        u[k,i] = rand(Normal(eta[k,i],1));
       end
 
     end
   end
 end
 
-#sample eta
-function sample_eta(u,X,SigmaB)
+function sample_B!(B,u,X,SigmaB)
   K = size(u)[1]+1;
-  eta = Array(Float64,K-1);
-  muB = SigmaB.*(u*X);
+
+  muB = SigmaB*X*u';
   for k in 1:(K-1)
-    eta[k] = rand(Normal(muB[k],sqrt(SigmaB[1])));
+    B[:,k] = rand(MvNormal(muB[:,k],SigmaB));
   end
 end
 
