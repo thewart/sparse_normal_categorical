b1ac4d6d476a4ed4470b8f9b5a92a66e7dfb0708
thewart <seth.madlonkay@gmail.com>
2016-02-19 18:13:47 -0500

    added naive bayes poisson clusters & improved notebook

--- normal_probit_gibbs.jl
+++ normal_probit_gibbs.jl
@@ -106,6 +106,33 @@ function sample_poisson(z,prior)
   return lambda;
 end
 
+#sample poisson set
+function sample_multipoisson(Y,z,prior)
+  K = prior[:K];
+  d = length(Y[1]);
+  n = length(z);
+  sz = zeros(Int64,d,K);
+  nz = zeros(Int64,K);
+  n = length(z);
+  lambda = Array(Float64,K);
+
+  for i in 1:n
+    k = z[i];
+    nz[k] += 1;
+    for j in 1:d
+      sz[k,j] += Y[i][j];
+    end
+  end
+
+  for k in 1:K
+    for j in 1:d
+      lambda[k] = rand(Gamma(prior[:a0] + sz[k], inv(prior[:b0] + nz[k])));
+    end
+  end
+
+  return lambda;
+end
+
 function sample_u!(u,z,eta)
   (K,n) = size(u);
   K += 1;
